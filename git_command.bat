20485c4dca60fde0e434029def546045c31424be
yinwun <liang_niki@hotmail.com>
2024-09-27 03:39:09 +0530

    Initial commit

--- git_command.bat
+++ git_command.bat
@@ -1,9 +1,15 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+:: Check if the path parameter is provided
+IF "%~1"=="" (
+    echo Usage: %0 "C:\path\to\your\repository"
+    exit /b
+)
+
 :: Get the current date in YYYY-MM-DD format
 FOR /F "tokens=2 delims==" %%I IN ('wmic os get localdatetime /value') DO SET datetime=%%I
-SET today=!datetime:~0,4!-!datetime:~4,2!-!datetime:~6,2!
+SET today=!datetime:~0,4!-!datetime:~4,2!-!datetime:~6,2!_!datetime:~8,2!-!datetime:~10,2!-!datetime:~12,2!-!datetime:~15,3!
 
 :: Set the branch name using the current date
 SET BRANCH_NAME=branch_!today!
